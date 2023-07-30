93cdbbec71c114a05179049799f933cf0a64e883
GITHARSH434 <105858398+GITHARSH434@users.noreply.github.com>
2023-07-30 17:38:23 +0530

    Update customer.php

--- customer.php
+++ customer.php
@@ -4,7 +4,6 @@
 
 <head>
   <title>Customer</title>
-  <!-- <link rel="stylesheet" href="login.css"> -->
   <style>
     body {
       margin: 0;
@@ -20,6 +19,7 @@
     ::-webkit-scrollbar {
       display: none;
     }
+
     .container {
       margin-top: 50px;
       padding-top: 200px;
@@ -44,19 +44,69 @@
       /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
       background: linear-gradient(to right bottom, rgba(246, 211, 101, 1), rgba(253, 160, 133, 1));
     }
-    #changebtn{
+
+    #changebtn {
       padding: 3px 11px;
-      border:2px Solid slateblue;
+      border: 2px solid slateblue;
       border-radius: 0px;
       background-color: rgba(255, 255, 255, 0.1);
       transition: 0.2s;
     }
-    #changebtn:hover{
+
+    #changebtn:hover {
       box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-      border:2px Solid dodgerblue;
+      border: 2px solid dodgerblue;
       background-color: rgba(255, 255, 255, 0.3);
     }
-    
+
+    .table2 {
+      table-layout: fixed;
+      width: 100%;
+    }
+
+    .changetable {
+      max-width: fit-content;
+      align-self: center;
+      justify-self: center;
+      background: #f6d365;
+      background: linear-gradient(to right bottom, rgba(246, 211, 101, 1), rgba(253, 160, 133, 1));
+      color: black;
+      padding: 15px 15px;
+      margin-top: 20px;
+      border-radius: 5px;
+      width: 100%;
+    }
+
+    .inputtable {
+      width: 100%;
+      border-radius: .5rem;
+      border: 2px solid white;
+      padding: 5px;
+    }
+
+    #changebtn2 {
+      padding: 3px 11px;
+      border: 2px solid slateblue;
+      border-radius: 0px;
+      background-color: rgba(255, 255, 255, 0.8);
+      transition: 0.2s;
+    }
+
+    #changebtn2:hover {
+      box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+      border: 2px solid dodgerblue;
+      background-color: rgba(255, 255, 255, 0.9);
+    }
+
+    @media (max-width: 768px) {
+      .changetable {
+        max-width: 80%;
+      }
+    }
+
+    html {
+      scroll-behavior: smooth;
+    }
   </style>
 </head>
 
@@ -101,9 +151,11 @@ if (!isset($_SESSION['name'])) {
             <div class="col-md-4 gradient-custom text-center text-white"
               style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
               <img src="img/user.png" alt="Avatar" class="img-fluid my-6" style="width: 80px;" />
-              <h5><?php echo $fn . " " . $ln; ?></h5>
+              <h5>
+                <?php echo $fn . " " . $ln; ?>
+              </h5>
               <form method="POST">
-              <button type="submit" id="changebtn">Change Details</button>
+                <button type="submit" name="submit" id="changebtn">Change Details</button>
               </form>
               <i class="far fa-edit mb-5"></i>
             </div>
@@ -114,18 +166,24 @@ if (!isset($_SESSION['name'])) {
                 <div class="row pt-1">
                   <div class="col-13 mb-3">
                     <h6>Email</h6>
-                    <p class="text-muted"><?php echo $email; ?></p>
+                    <p class="text-muted">
+                      <?php echo $email; ?>
+                    </p>
                   </div>
                 </div>
-                
+
                 <div class="row pt-1">
                   <div class="col-6 mb-3">
                     <h6>Date of Birth</h6>
-                    <p class="text-muted"><?php echo $dob; ?></p>
+                    <p class="text-muted">
+                      <?php echo $dob; ?>
+                    </p>
                   </div>
                   <div class="col-6 mb-3">
                     <h6>Phone</h6>
-                    <p class="text-muted"><?php echo $no; ?></p>
+                    <p class="text-muted">
+                      <?php echo $no; ?>
+                    </p>
                   </div>
                 </div>
               </div>
@@ -134,6 +192,92 @@ if (!isset($_SESSION['name'])) {
         </div>
       </div>
     </div>
+    <?php
+    if (isset($_POST['submit'])) { ?>
+      <center>
+        <div class="changetable col-lg-6 mb-1 mb-lg-0">
+          <form method="POST" action="">
+            <table class="table2">
+              <tr>
+                <p class="texttable text-center"><strong>Change Information</strong><br>
+                  <hr class="p-0 mt-0">
+                </p><br>
+                <th style="width: 50%;">First Name</th>
+                <th style="width: 50%;">Last Name</th>
+              </tr>
+              <tr>
+                <td><input type="text" class="inputtable" name="FN"></input></td>
+                <td><input type="text" class="inputtable" name="LN"></input></td>
+              </tr>
+              <tr>
+                <th style="width: 50%;">Phone</th>
+                <th style="width: 50%;">Date of Birth</th>
+              </tr>
+              <tr>
+                <td><input type="contact" class="inputtable" name="PHONE"></input></td>
+                <td><input type="date" class="inputtable" name="DATE"></input></td>
+              </tr>
+              <tr>
+                <td><br>
+                  <button type="submit" name="submit2" id="changebtn2">Submit</button>
+                </td>
+              </tr>
+            </table>
+          </form>
+        </div>
+      </center>
+      <?php
+      if (isset($_POST['submit2'])) {
+        $newFirstName = $_POST['FN'];
+        $newLastName = $_POST['LN'];
+        $newPhone = $_POST['PHONE'];
+        $newDateOfBirth = $_POST['DATE'];
+    
+        // Prepare the SQL statement with placeholders for variables
+        $sql = "UPDATE customer SET ";
+        $params = array();
+    
+        if (!empty($newFirstName)) {
+            $sql .= "firstname = :firstname, ";
+            $params[':firstname'] = $newFirstName;
+        }
+    
+        if (!empty($newLastName)) {
+            $sql .= "lastname = :lastname, ";
+            $params[':lastname'] = $newLastName;
+        }
+    
+        if (!empty($newPhone)) {
+            $sql .= "contactno = :contactno, ";
+            $params[':contactno'] = $newPhone;
+        }
+    
+        if (!empty($newDateOfBirth)) {
+            $sql .= "dob = :dob, ";
+            $params[':dob'] = $newDateOfBirth;
+        }
+    
+        // Remove the trailing comma and space
+        $sql = rtrim($sql, ', ');
+    
+        // Add the WHERE clause to update the specific customer
+        $sql .= " WHERE customerid = :customerid";
+        $params[':customerid'] = $Id;
+    
+        try {
+            // Prepare and execute the UPDATE query
+            $stmt = $conn->prepare($sql);
+            $stmt->execute($params);
+    
+            echo "Customer information updated successfully.";
+        } catch (PDOException $e) {
+            echo "Error updating customer information: " . $e->getMessage();
+        }
+    }
+    }
+    ?>
+
+
   </div>
 
 </body>
