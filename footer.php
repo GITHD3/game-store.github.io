e35be303a81726290964753c5a04d17989433e67
GITHD3 <117884951+GITHD3@users.noreply.github.com>
2023-08-09 11:55:09 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,56 +5,62 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+  <style>
+    body {
+      margin: 0;
+      padding-top: 8px;
+      display: flex;
+      justify-content:center;
+      flex-direction: column;
+      min-height: 100vh;
+    }
+
+    footer {
+      background-color: #333333;
+      width: 100%;
+      
+      padding: 1rem;
+      color: white;
+      text-align: center;
+      margin-top: auto; /* Push the footer to the bottom */
+    }
+
+    .btnFOOTER {
+      border-radius: 100%;
+      border: none;
+      padding: 15px;
+    }
+
+    .btnFOOTER:hover {
+      border-radius: 100%;
+      box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+    }
+    .suppose{
+      border : 1px solid #171717;
+      border-radius:8px;
+    }
+  </style>
 </head>
-<style>
-  footer {
-    background-color: #333333;
-    margin-top: 10px;
-    bottom: 0;
-    width: 98%;
-  }
-  
-  body {
-    justify-self: center;
-    position: relative;
-    min-height: 100vh;
-  }
 
-  .btnFOOTER {
-    border-radius: 100%;
-    border: none;
-    padding: 15px;
-  }
-
-  .btnFOOTER:hover {
-    border-radius: 100%;
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
-  }
-</style>
-
-<body >
-  <footer class="bg text-center text-white">
+<body>
+  <footer>
     <div class="p-4 pb-0">
       <section class="mb-4">
-
-        <!-- Google -->
         <a class="btnFOOTER text-white btn-floating m-1" style="background-color: #dd4b39;" target="_blank"
           href="index.php" role="button"><i class="fab fa-google"></i></a>
 
-        <!-- Instagram -->
         <a class="btnFOOTER text-white btn-floating m-1" style="background-color: #ac2bac;" target="_blank"
           href="https://www.instagram.com/thisis__harsh/" role="button"><i class="fab fa-instagram"></i></a>
 
-        <!-- Linkedin -->
         <a class="btnFOOTER text-white btn-floating m-1" style="background-color: #0082ca;" target="_blank"
           href="https://www.linkedin.com/in/harsh-dedakiya-93694a269/" role="button"><i
             class="fab fa-linkedin-in"></i></a>
-        <!-- Github -->
+
         <a class="btnFOOTER text-white btn-floating m-1" style="background-color: #333333;" target="_blank"
           href="https://github.com/GITHD3" role="button"><i class="fab fa-github"></i></a>
       </section>
     </div>
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    <div class="text-center p-3 suppose" style="background-color: rgba(0, 0, 0, 0.2);">
       © 2020 Copyright:
       <a class="text-white" href="index.php">ParaCrash Game Store</a>
     </div>
