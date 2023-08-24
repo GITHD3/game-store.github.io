149a640d2aa7a91bcf3630fd1ef11cd2c2927dc2
GITHD3 <117884951+GITHD3@users.noreply.github.com>
2023-08-24 12:15:56 +0530

    Update seppage.php

--- seppage.php
+++ seppage.php
@@ -19,7 +19,7 @@ $database = "game4";
 $conn = new PDO("mysql:host=$host;dbname=$database", $username, $db_password);
 
 
-$stmt = $conn->prepare("SELECT `gamename`, `developer_name`, `publisher_name`, `genre_name`, `price`, `mature_content`, `gamesize`, `gametype`, `description`, 
+$stmt = $conn->prepare("SELECT `gameid`,`gamename`, `developer_name`, `publisher_name`, `genre_name`, `price`, `mature_content`, `gamesize`, `gametype`, `description`, 
 `memory_required`, `operating_system`, `processor_required`, `storage_required`, `release_date` FROM `games` WHERE gamename = :tmp ");
 $stmt->bindParam(":tmp", $gameName);
 $stmt->execute();
@@ -27,6 +27,7 @@ $stmt->execute();
 $games = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if ($games) {
+    $gameidtemp = $games['gameid'];
     $gamename = $games['gamename'];
     $dname = $games['developer_name'];
     $pname = $games['publisher_name'];
@@ -72,83 +73,105 @@ if ($games) {
     font-family: 'Press Start 2P';font-size: 22px; color:black;"><strong>ParaCrash Game Store</strong></h4>
     </DIV>
     <STYLE>
-        body {
-            background: linear-gradient(#404ccc, #03e9f4);
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-            height: 100% !important;
-            overflow-y: smooth;
-        }
+    body {
+        background: linear-gradient(#404ccc, #03e9f4);
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        height: 100% !important;
+        overflow-y: smooth;
+    }
 
-        /* Hide scrollbar */
-        ::-webkit-scrollbar {
-            display: none;
-        }
+    /* Hide scrollbar */
+    ::-webkit-scrollbar {
+        display: none;
+    }
 
-        .card {
-            max-width: 260px;
-            display: inline-block;
-        }
+    .card {
+        max-width: 260px;
+        display: inline-block;
+    }
+    .card2{
+        display: table;
+    }
 
-        .scrolling-wrapper {
-            overflow-x: scroll;
-            overflow-y: hidden;
-            white-space: nowrap;
-        }
+    .scrolling-wrapper {
+        overflow-x: scroll;
+        overflow-y: hidden;
+        white-space: nowrap;
+    }
     </STYLE>
 
 
 
 
     <style>
-        .fakeimg1 {
-            height: 200px;
-            max-width: 210px;
-            border-radius: 4px;
-            border: 1px solid black;
-        }
+    .fakeimg1 {
+        height: 200px;
+        max-width: 210px;
+        border-radius: 4px;
+        border: 1px solid black;
+    }
 
-        .fakeimg {
-            height: 200px;
-            max-width: 200px;
+    .fakeimg {
+        height: 200px;
+        max-width: 200px;
 
-        }
+    }
 
-        #all {
-            outline: rgb(255, 255, 159);
-            outline-style: solid;
-            border-radius: 4px;
-        }
+    #all {
+        outline: rgb(255, 255, 159);
+        outline-style: solid;
+        border-radius: 4px;
+    }
 
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-            font-family: 'Nohemi', sans-serif;
-        }
-        p{
-            font-family: 'satoshi' ,sans-serif;
-        }
-        .special {
-            font-family: 'Aspekta', sans-serif;
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        font-family: 'Nohemi', sans-serif;
+    }
 
-            align-items: center;
-        }
+    p {
+        font-family: 'satoshi', sans-serif;
+    }
 
-        #special2 {
-            font-size: 22px;
-        }
+    .special {
+        font-family: 'Aspekta', sans-serif;
 
-        #special3 {
-            font-style: none;
-            font-size: 19px;
-        }
+        align-items: center;
+    }
 
-        #line1 {
-            color: yellow;
-        }
+    #special2 {
+        font-size: 22px;
+    }
+
+    #special3 {
+        font-style: none;
+        font-size: 19px;
+    }
+
+    #line1 {
+        color: yellow;
+    }
+
+    .btn {
+        text-align: center;
+        background-color: black;
+        color: beige;
+        outline: 2px solid black;
+        border-radius: 4px;
+        transition: 0.5s;
+        width: 146px;
+    }
+
+    .btn:hover {
+        background-color: #33363D;
+        border-radius: 7px;
+        color: blanchedalmond;
+        box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+    }
     </style>
 
 
@@ -159,8 +182,74 @@ if ($games) {
             <div class="col-sm-4">
 
                 <br>
-                <div class="card p-0 m-0"> <img class="fakeimg1" src="img/<?php echo $gameName; ?>.webp"> </div>
-                <br>
+                <div class="card p-0 m-0">
+                    <img class="fakeimg1" src="img/<?php echo $gameName; ?>.webp">
+                </div>
+                        <div class="card2 p-0 pt-2 m-0">
+                <?php
+                    $check_game_cart = "SELECT * FROM `cart` WHERE gameid = :gameid";
+                                        $stmt_check_game_cart = $conn->prepare($check_game_cart);
+                                        $stmt_check_game_cart->bindParam(':gameid', $gameidtemp);
+                                        $stmt_check_game_cart->execute();
+                                        if ($stmt_check_game_cart->rowCount() > 0) {
+                                            echo '
+                                                <form class="formbutton" method="POST" action="cart.php">
+                                                <input type="hidden" name="gameid" value="<?php echo $gameidtemp; ?>">
+                <button type="submit" class="btn" name="go_to_cart">Go to Cart</button>
+                </form>
+                ';
+                } else {
+                echo '
+                <form class="formbutton" method="POST" id="add-to-cart-form">
+                <input type="hidden" name="gameid" value="<?php echo $gameidtemp; ?>">
+                <button type="submit" class="btn" name="add_to_cart" id="add-to-cart-button">Add to Cart</button>
+            </form>
+';
+                if (isset($_POST['add_to_cart'])) {
+                if (isset($_SESSION['id'])) {
+                $cartid = $_SESSION['id'];
+
+                $addquery = "INSERT INTO `cart`(`cartid`, `gameid`, `amount`) VALUES
+                (:cartid, :gameid, 0)";
+
+                $stmt = $conn->prepare($addquery);
+                $stmt->bindParam(':cartid', $cartid);
+                $stmt->bindParam(':gameid', $gameidtemp);
+
+                try {
+                $stmt->execute();
+                } catch (PDOException $e) {
+                }
+                } else {
+                ?>
+                <script>
+                Swal.fire({
+                    text: "It looks like you're trying to add an item to your cart. To start shopping with us and enjoy all the benefits, please sign up or create an account.",
+                    showDenyButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'Login',
+                    denyButtonText: 'Sign In',
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = 'login.php';
+                    } else if (result.isDenied) {
+                        window.location.href = 'registeration.php';
+                    } else {
+                        Swal.fire('Continue as Visitor', '', 'info');
+                    }
+                });
+                </script>
+                <?php
+                                                }    }}
+                                            ?>
+
+                
+                
+                <form class="formbutton text-center p-0 m-0 pt-2" method="POST" action="seppage.php">
+                                            <input type="hidden" value="<?php echo $game['gamename']; ?>" name="gameName">
+                                            <button type="submit" class="btn">Buy</button>
+                                        </form>
+                </div>
                 <br>
                 <p>
                     <?php
@@ -236,7 +325,29 @@ if ($games) {
 </body>
 
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#add-to-cart-button').click(function() {
+            var gameidtemp = <?php echo json_encode($gameidtemp); ?>;
+            $.ajax({
+                type: "POST",
+                url: "cart.php", // Replace with your PHP script handling the cart addition
+                data: { gameid: gameidtemp },
+                success: function(response) {
+                    console.log(response); // Print the response for debugging
+                    // Assuming response contains updated content in HTML format
+                    $('#update-section').html(response); // Update the content on the page
+                },
+                error: function(xhr, status, error) {
+                    console.log(error); // Log any error for debugging
+                }
+            });
+        });
+    });
+</script>
+
 
 
 </html>
-<?php include 'footer.php'; ?>
+<?php  include 'footer.php'; ?>
